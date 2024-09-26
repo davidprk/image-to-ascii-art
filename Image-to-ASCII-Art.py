9211b0da301f45577b37a098260ce130af0d9d75
David Park <DavidPark2027@u.northwestern.edu>
2024-09-25 22:45:29 -0500

    Receiving user input

--- Image-to-ASCII-Art.py
+++ Image-to-ASCII-Art.py
@@ -1,1 +1,21 @@
+# Importing Pillow (image library)
+from PIL import Image, ImageEnhance
    
+#
+# Receiving user input
+#
+valid_pathname = False
+while not valid_pathname:
+    
+    try:
+        print("Enter a valid pathname to an image: ")
+        
+        pathname = input()
+        
+        image = Image.open(pathname)
+    except:
+        print("Given invalid pathname. Please try again.")
+    else:
+        valid_pathname = True
+
+# ASCII Chars from darkest to lightest: $@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'.
